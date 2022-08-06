7c6d87aeb80e4614b1a69d4d7f2d5f484d709b53
Yaroslav Zelenyi <yaroslagreen@gmail.com>
2022-08-06 20:01:43 +0300

    NavSearch has finished

--- src/Components/Search-menu/SearchMenu.js
+++
@@ -1,8 +0,0 @@
-import './searchMenu.scss';
-import main_logo from '../../resources/icons/dat_logo.svg';
-const SearchMenu = ()=>{
-    return(
-        <a href="#"><img src={main_logo} alt="logo" /></a>
-    )
-}
-export default SearchMenu;
