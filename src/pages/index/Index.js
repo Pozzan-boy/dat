2d79fc61a5386b66dc97a1d86d65b54dff74d197
Yaroslav Zelenyi <yaroslagreen@gmail.com>
2022-09-23 13:52:14 +0300

    Buttons bar added

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -2,13 +2,14 @@ import NavMenu from "../../Components/Nav-menu/NavMenu";
 import NavSearch from "../../Components/NavSearch/NavSearch";
 import Header from "../../Components/Header/Header";
 import AboutUs from "../../Components/AboutUs/AboutUs";
-
+import NavButtons from "../../Components/NavButtons/NavButtons";
 const Index = () => {
     return (
         <>
             <NavMenu />
             <Header>
                 <NavSearch/>
+                <NavButtons/>
             </Header>
             <AboutUs />
         </>
